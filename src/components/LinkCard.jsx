26675461bd090576062ac9c9072fae234d9923e5
Anusha-Pwr <anusha00466@gmail.com>
2024-08-22 16:09:26 +0530

    add check icon feedback for url copy

--- src/components/LinkCard.jsx
+++ src/components/LinkCard.jsx
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
-import { Copy, Download, Trash } from "lucide-react";
+import { Check, Copy, Download, Trash } from "lucide-react";
 import useFetch from "../hooks/useFetch";
 import { deleteUrl } from "../db/apiUrls";
 import { BeatLoader } from "react-spinners";
 
 const LinkCard = ({ url, fetchUrls }) => {
+  const {
+    data,
+    loading,
+    error,
+    fn: fnDeleteUrl,
+  } = useFetch(deleteUrl, url?.id);
 
-    const {data, loading, error, fn:fnDeleteUrl} = useFetch(deleteUrl, url?.id);
+  const [copied, setCopied] = useState(false);
+  const copiedTimeOutRef = useRef();
 
-    function downloadImage() {
-        const imgUrl = url?.qr;
-        const fileName = url?.title;
-
-        const anchor = document.createElement("a");
-        anchor.href = imgUrl;
-        anchor.download = fileName;
-
-        document.body.appendChild(anchor);
-
-        anchor.click();
-        document.body.removeChild(anchor);
+  useEffect(() => {
+    return () => {
+        if(copiedTimeOutRef.current) {
+            clearTimeout(copiedTimeOutRef.current);
+        }
     }
+  }, []);
+
+  function downloadImage() {
+    const imgUrl = url?.qr;
+    const fileName = url?.title;
+
+    const anchor = document.createElement("a");
+    anchor.href = imgUrl;
+    anchor.download = fileName;
+
+    document.body.appendChild(anchor);
+
+    anchor.click();
+    document.body.removeChild(anchor);
+  }
+
+  function handleCopyUrl() {
+    navigator.clipboard.writeText(`https://clipper.in/${url?.short_url}`);
+    setCopied(true);
+
+    if(copiedTimeOutRef.current) {
+        clearTimeout(copiedTimeOutRef.current);
+    }
+
+    copiedTimeOutRef.current = setTimeout(() => {
+      setCopied(false);
+    }, 500);
+  }
 
   return (
     <div className="flex flex-col md:flex-row gap-5 border p-4 bg-gray-900 rounded-lg">
@@ -32,7 +60,7 @@ const LinkCard = ({ url, fetchUrls }) => {
         className="h-32 object-contain ring ring-blue-500"
       />
       <Link to={`/link/${url?.id}`} className="flex flex-col flex-1">
-        <span className="text-3xl font-extrabold hover:underline cursor-pointer">
+        <span className="text-3xl font-extrabold hover:underline cursor-pointer mt-0">
           {url?.title}
         </span>
         <span className="text-2xl text-blue-400 font-bold hover:underline cursor-pointer">
@@ -41,23 +69,31 @@ const LinkCard = ({ url, fetchUrls }) => {
         <span className="flex items-center gap-1 hover:underline cursor-pointer">
           {url?.original_url}
         </span>
-        <span className="flex items-end font-extralight text-sm flex-1">
-          {new Date(url?.created_at).toLocaleString()}
+        <span className="flex items-end font-extralight text-xs flex-1">
+          Created: {new Date(url?.created_at).toLocaleString()}
         </span>
+        {url?.expiration_date && (
+          <span className="flex items-end font-extralight text-xs text-red-500">
+            Expired: {new Date(url?.expiration_date).toLocaleString()}
+          </span>
+        )}
       </Link>
 
       <div className="flex gap-2">
-        <Button variant="ghost" onClick={() => navigator.clipboard.writeText(`https://clipper.in/${url?.short_url}`)}>
-            <Copy />
+        <Button variant="ghost" onClick={handleCopyUrl}>
+          {copied ? <Check className="text-green-500" /> : <Copy />}
         </Button>
         <Button variant="ghost" onClick={downloadImage}>
-            <Download />
+          <Download />
         </Button>
-        <Button variant="ghost" onClick={async() => {
+        <Button
+          variant="ghost"
+          onClick={async () => {
             await fnDeleteUrl();
             fetchUrls();
-        }}>
-            {loading ? <BeatLoader color="white" size={5} /> : <Trash />}
+          }}
+        >
+          {loading ? <BeatLoader color="white" size={5} /> : <Trash />}
         </Button>
       </div>
     </div>
